f3ff448c57ac65eca024baac04d45abb9cd68c8f
Aidan Tay <silver14211@gmail.com>
2024-05-28 15:23:14 +0800

    Support for tap complete questions

--- questiontypes.py
+++ questiontypes.py
@@ -19,7 +19,7 @@ class Question:
         # initializes correct question type class given duolingo's question type
         # OR if question was seen before, return the question object
 
-        questionsMap = {"challenge challenge-select": SelectionQuestion, "challenge challenge-translate": TranslationQuestion, "challenge challenge-listenTap": ListenTapQuestion}
+        questionsMap = {"challenge challenge-select": SelectionQuestion, "challenge challenge-translate": TranslationQuestion, "challenge challenge-listenTap": ListenTapQuestion, "challenge challenge-tapComplete": TapCompleteQuestion}
         if newQuestionType not in questionsMap:
             raise Exception(f"Question Type not found: {newQuestionType}")
         
@@ -91,6 +91,7 @@ class TranslationQuestion(Question):
         self.clickNext()
 
     def solve(self):
+        # matches largest choice to target string
         choices = self.driver.find_elements(By.XPATH, "//span[@data-test='challenge-tap-token-text']").sort(key=lambda x:len(x.text))
         target = " ".join([s.strip(",.!?:;") for s in self.answer.split()])
 
@@ -104,7 +105,37 @@ class TranslationQuestion(Question):
                 break
         self.clickNext()
 
+
 class ListenTapQuestion(Question):
     questionType = "Listen"
     def guess(self):
+        # just skip the question
         self.driver.find_element(By.XPATH, "//button[@data-test='player-skip']").click()
+
+
+class TapCompleteQuestion(Question):
+    questionType = "Tap Complete"
+    def recordQuestion(self):
+        tmp = self.driver.find_elements(By.XPATH, "//span[@class='_5HFLU']/span/span")
+        self.questionData = "".join([x.text for x in tmp])
+
+    def guess(self):
+        self.driver.find_element(By.XPATH, "//span[@data-test='challenge-tap-token-text']").click()
+        self.clickNext()
+
+    def solve(self):
+        choices = self.driver.find_elements(By.XPATH, "//span[@data-test='challenge-tap-token-text']").sort(key=lambda x:len(x.text))
+
+        i = 0
+        while i < len(self.answer):
+            while self.answer[i] == self.questionData[i]:
+                i += 1
+
+            for j, choice in enumerate(choices):
+                if choice.text == self.answer[i:i+len(choice.text)]:
+                    choice.click()
+                    i += len(choice.text)
+                    choices.pop(j)
+                break
+
+        self.clickNext()
